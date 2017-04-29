d6b299a0b2e0965a232762880a2c88a978985578
Manos <manos.t@hotmail.com>
2017-04-29 17:59:38 +0300

    kmeans-stats-start

--- kmeans.py
+++ kmeans.py
@@ -6,9 +6,11 @@ from sklearn.decomposition import TruncatedSVD
 import nltk
 from sklearn.cluster import KMeans
 
-pathToFile = '/home/lef/Desktop/train_set.csv'
+pathToFile = '../train_set.csv'
 
 df = pd.read_csv(pathToFile, sep='\t')
+A = np.array(df)
+
 stop_words=['Antonia','Nikos','Nikolas']
 count_vect = CountVectorizer(stop_words = stop_words)
 train_counts = count_vect.fit_transform(df.Content)
@@ -17,13 +19,33 @@ svd = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
 print (train_counts)
 X = svd.fit(train_counts)
 km = KMeans(n_clusters=5, random_state=1).fit(svd.transform(train_counts))
-k = 5
-#km.fit(svd.transform(train_counts))
-labels = km.labels_
- 
-	# Sum of distances of samples to their closest cluster center
-interia = km.inertia_
-print ("k:",k, " cost:", interia)
+
+cluster_stats = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+labels = km.labels_.tolist()
+
+for i,label in enumerate(labels):
+	if (A[i][3] is "Politics"):
+		cluster_stats[label][0] += 1
+		cluster_stats[label][5] += 1
+	elif (A[i][3] is "Film"):
+		cluster_stats[label][1] += 1
+		cluster_stats[label][5] += 1
+	elif (A[i][3] is "Footbal"):
+		cluster_stats[label][2] += 1
+		cluster_stats[label][5] += 1
+	elif (A[i][3] is "Business"):
+		cluster_stats[label][3] += 1
+		cluster_stats[label][5] += 1
+	else: #Technology
+		cluster_stats[label][4] += 1
+		cluster_stats[label][5] += 1
+
+categories = ['Politics', 'Film', 'Football', 'Business', 'Technology']
+
+for i in range(5):
+	print ("Cluster ", i)
+	for j,cat in enumerate(categories):
+		print ('\t', cat, ': ', cluster_stats[i][j] / cluster_stats[i][5], '%')
 
 #the function that finds the distance (metric function) is passed to KmeansClusterer
 #kmeansClusterer = nltk.cluster.KMeansClusterer(num_means=5, distance=cosine_similarity)
