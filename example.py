6966046f3a5c9030ce500c9dfeaa71e7e8db1c8e
LefterisKarampas <sdi1400064@di.uoa.gr>
2017-04-29 16:51:30 +0300

    manos_gleiftra

--- example.py
+++ example.py
@@ -4,29 +4,25 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import TruncatedSVD
 import nltk
+ 
+# The kmeans algorithm is implemented in the scikits-learn library
 from sklearn.cluster import KMeans
-
+ 
+ 
 pathToFile = '/home/lef/Desktop/train_set.csv'
-
+ 
 df = pd.read_csv(pathToFile, sep='\t')
 stop_words=['Antonia','Nikos','Nikolas']
 count_vect = CountVectorizer(stop_words = stop_words)
-train_counts = count_vect.fit_transform(df.Content)
 svd = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
-#train_counts.shape
-print (train_counts)
-X = svd.fit(train_counts)
-km = KMeans(n_clusters=5, random_state=1).fit(svd.transform(train_counts))
-k = 5
-#km.fit(svd.transform(train_counts))
-labels = km.labels_
+count_vect.fit(df.Content)
+train_counts = count_vect.transform(df.Content)
+	# Create a kmeans model on our data, using k clusters.  random_state helps ensure that the algorithm returns the same results each time.
+kmeans_model = KMeans(n_clusters=5, random_state=1).svd.fit(df.Content)
+	
+	# These are our fitted labels for clusters -- the first cluster has label 0, and the second has label 1.
+labels = kmeans_model.labels_
  
 	# Sum of distances of samples to their closest cluster center
-interia = km.inertia_
+interia = kmeans_model.inertia_
 print ("k:",k, " cost:", interia)
-
-#the function that finds the distance (metric function) is passed to KmeansClusterer
-#kmeansClusterer = nltk.cluster.KMeansClusterer(num_means=5, distance=cosine_similarity)
-#kmeansClusterer.cluster(train_counts, True)
-
-#kmeans_mode = Kmeans(n_clusters=5, random_state=0).fit(df.iloc[:, :])
