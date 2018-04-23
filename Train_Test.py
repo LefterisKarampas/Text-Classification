1a99a22504cb56ec29f18e740c24f3b444da2f6b
LefterisKarampas <mr.lef_21113@hotmail.com>
2018-04-23 20:26:11 +0300

    Train_Test

--- Train_Test.py
+++ Train_Test.py
@@ -10,14 +10,16 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 from sklearn import preprocessing
 from sklearn.cross_validation import train_test_split
-
-
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_val_score
+import knn
 
 #Read Train Data
 data = pd.read_csv('../datasets/train_set.csv', sep="\t")
-data = data[0:400]
+data = data[0:1000]
 
-train_data ,test_data = train_test_split(data,test_size=0.4)
+train_data ,test_data = train_test_split(data,test_size=0.3)
 
 #Initialize Encoder
 le = preprocessing.LabelEncoder()
@@ -34,31 +36,45 @@ X = tfid_vectorizer.fit_transform(train_data['Content']+5*train_data['Title'])
 #Y = count_vectorizer.transform(test_data['Content']+5*test_data['Title'])
 Y = tfid_vectorizer.transform(test_data['Content']+5*test_data['Title'])
 
+
+#LSA
+lsa = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
+Scaler = MinMaxScaler(copy=True, feature_range=(50, 100))
+lsa_X = lsa.fit_transform(X)
+lsa_Y = lsa.fit_transform(Y)
+Scaler_X = Scaler.fit_transform(lsa_X)
+Scaler_Y = Scaler.fit_transform(lsa_Y)
+
+
 #Train classifiers
 rclf = RandomForestClassifier()
-rclf.fit(X, y)
+rclf.fit(lsa_X, y)
 
-mclf = MultinomialNB()
-mclf.fit(X,y)
+mclf = MultinomialNB(alpha=0.01)
+mclf.fit(Scaler_X,y)
 
 sclf = svm.SVC(kernel='linear', C = 1)
-sclf.fit(X,y)
+sclf.fit(lsa_X,y)
 
 #Random_Forest
-RFy_pred = rclf.predict(Y)
+RFy_pred = rclf.predict(lsa_Y)
 predicted_categories = le.inverse_transform(RFy_pred)
 print classification_report(y_test, RFy_pred, target_names=list(le.classes_))
 
 
 #MultinomiaNB
-MNBy_pred = mclf.predict(Y)
+MNBy_pred = mclf.predict(Scaler_Y)
 predicted_categories = le.inverse_transform(MNBy_pred)
 print classification_report(y_test, MNBy_pred, target_names=list(le.classes_))
 
 
 #SVM
-SVMy_pred = sclf.predict(Y)
+SVMy_pred = sclf.predict(lsa_Y)
 predicted_categories = le.inverse_transform(SVMy_pred)
 print classification_report(y_test, SVMy_pred, target_names=list(le.classes_))
 
-
+myknn = knn.KNN(5)
+myknn.fit(lsa_X,y)
+KNN_pred = myknn.predict(lsa_Y)
+predicted_categories = le.inverse_transform(KNN_pred)
+print classification_report(y_test, KNN_pred, target_names=list(le.classes_))
