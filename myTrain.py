c7a72563cbe2d17ae3c4469733b7a0a0842f6ddf
LefterisKarampas <mr.lef_21113@hotmail.com>
2018-04-30 21:13:01 +0300

    myTrain

--- myTrain.py
+++ myTrain.py
@@ -21,10 +21,15 @@ from nltk.corpus import stopwords
 from preprocessing import *
 
 #Read Train Data
-data = pd.read_csv('../datasets/train_set.csv', sep="\t")
-data = data[0:2000]
+#data = pd.read_csv('../datasets/train_set.csv', sep="\t")
+#data = data[0:2000]
 
-train_data ,test_data = train_test_split(data,test_size=0.3)
+train_data = pd.read_csv('../datasets/train_set.csv', sep="\t")
+#train_data = train_data[0:2000]
+
+test_data = pd.read_csv('../datasets/test_set.csv', sep="\t")
+
+#train_data ,test_data = train_test_split(data,test_size=0.3)
 
 #Initialize Encoder
 le = preprocessing.LabelEncoder()
@@ -37,13 +42,23 @@ train_text = train_data['Content']+titleWeight*(" " + train_data['Title'])
 text_text = test_data['Content']+titleWeight*(" " + test_data['Title'])
 
 train_X, test_X = stem_and_tfidf(train_text, text_text)
-features_num = 2000
-train_X, test_X = select_features(train_X, y, test_X, features_num)
+# features_num = 2000
+# tX = train_X
+# tY = test_X
+# for features_num in range(2000,10000,500):
+# 	print(features_num)
+# 	train_X, test_X = select_features(tX, y, tY, features_num)
 
 #SVM
 sclf = svm.SVC(kernel='linear', C = 301,gamma=0.01)
 sclf.fit(train_X,y)
 SVMy_pred = sclf.predict(test_X)
 predicted_categories = le.inverse_transform(SVMy_pred)
-print classification_report(y_test, SVMy_pred, target_names=list(le.classes_))
+#print classification_report(y_test, SVMy_pred, target_names=list(le.classes_))
 
+with open('testSet_categories.csv', 'wb') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow(['Id','Category'])
+    for i in range(len(test_data['Id'])):
+      csvwriter.writerow([str(test_data['Id'][i]),predicted_categories[i]])
